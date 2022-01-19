beeaca2be98356092e44e2dccd2311eb9150ba52
sinaya naik <sinayanaik@gmail.com>
2022-01-19 02:10:31 -0500

    couldn't figure out how to rotate arrow

--- options.js
+++ options.js
@@ -2,21 +2,31 @@ const options = document.getElementById("options");
 const add = document.getElementById("add");
 const remove = document.getElementById("remove");
 
-let option_no = 1;
+//empty arrow function
+const empty = arr => arr.length = 0;
+
+
+let option_no = 2;
 
 function Add_element() {
   option_no++;
   let input = document.createElement("input");
   input.type = "text";
   input.name = `option-${option_no}`;
+  input.id = `option-${option_no}`;
   input.className = "option";
   options.appendChild(input);
   options.appendChild(document.createElement("br"));
+  
+  empty(options_list)
 }
 
 function Remove_element() {
-  options.removeChild(options.lastChild);
-  options.removeChild(options.lastChild);
+  options.removeChild(options.lastElementChild)
+  options.removeChild(options.lastElementChild)
+  
+  empty(options_list)
+
 
   option_no--;
   if (option_no <= 0) {
--- sketch.js
+++ sketch.js
@@ -1,5 +1,26 @@
 const option = document.getElementsByClassName("option");
+const start = document.getElementById("start")
 let colors = [];
+let options_list = []
+
+function option_list(x,y,text){
+  this.start_theta = x
+  this.end_theta = y
+  this.text = text
+}
+class Arrow{
+  constructor(theta,r){
+    this.theta = theta
+    this.r =r 
+  }
+  rotate(){
+    this.theta += Math.floor(Math.random() * (9890 - 2312 + 1)) + 2312;
+
+  }
+}
+
+arrow = new Arrow(0,570)
+
 for(let i=0;i<15;i++){
   colors.push(randomColor({
     format: 'rgb'
@@ -12,12 +33,23 @@ function setup() {
   angleMode(DEGREES);
   background(0, 0, 0);
   //
+  Draw_circle_Update_list()
+
+  
 
 }
 
 function draw() {
   angleMode(DEGREES);
   translate(width / 2, height / 2);
+  add.addEventListener("click", Draw_circle_Update_list);
+  remove.addEventListener("click", Draw_circle_Update_list);
+  
+  line(0, 0, arrow.r * sin(arrow.theta), arrow.r * cos(arrow.theta))
+  arrow.rotate(2)
+
+}
+function Draw_circle_Update_list(){
   background(0, 0,0)
   //
   let theta = 360 / option_no;
@@ -27,8 +59,10 @@ function draw() {
   for (i = 1; i <= option_no; i++) {
     fill(colors[i]);
     arc(0, 0, 600, 600, start, end); // arc(center_x,centre_y,r_x,r_y,start_angle,end_angle)
+    var text= document.getElementById(`option-${i}`).value;
+    options_list.push(new option_list(start,end,text))
     start += theta;
     end += theta;
   }
-  rotate(20)
+  
 }
